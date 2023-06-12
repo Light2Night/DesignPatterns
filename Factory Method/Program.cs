404730341f94f3cd919879dfb5dfaf9cc5f416f8
3885vlad <3885vlad@gmail.com>
2023-06-12 14:18:41 +0300

    Add Factory Method

--- Factory Method/Program.cs
+++ Factory Method/Program.cs
@@ -1,7 +1,54 @@
-﻿namespace Factory_Method {
-	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello, World!");
-		}
+﻿namespace Factory_Method;
+
+interface IVehicle {
+	public void Drive();
+}
+
+class Car : IVehicle {
+	public void Drive() { }
+}
+
+class Bus : IVehicle {
+	public void Drive() { }
+}
+
+
+
+interface IFactory {
+	public IVehicle Build();
+}
+
+abstract class Factory : IFactory {
+	public IVehicle Build() {
+		IVehicle vehicle = CreateVehicle();
+		// Some configurations
+		return vehicle;
+	}
+
+	protected abstract IVehicle CreateVehicle();
+}
+
+class CarFactory : Factory {
+	protected override IVehicle CreateVehicle() {
+		return new Car();
+	}
+}
+
+class BusFactory : Factory {
+	protected override IVehicle CreateVehicle() {
+		return new Bus();
+	}
+}
+
+
+
+class Program {
+	static void Main(string[] args) {
+		IFactory factory;
+
+		factory = new CarFactory();
+		factory = new BusFactory();
+
+		IVehicle vehicle = factory.Build();
 	}
 }
